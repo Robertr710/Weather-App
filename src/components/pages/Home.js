20fd3406e67cca726eec8a0070a4b151e605ad0e
Robert Rodriguez <rrodriguez45@csustan.edu>
2020-08-22 22:18:31 -0700

    Pinging geocode API

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,22 +1,34 @@
   
-import React from 'react'
+import React, {useState} from 'react';
 import { Input, FormControl, InputLabel, FormHelperText, Button } from '@material-ui/core';
 
 
 
 
-export const Home = () => (
-  <div>
+export const Home = () => {
+  const [address, setAddress]= useState("");
+  const changeAddress = (e) => {
+    setAddress(e.target.value)
+  }
+  const Get = async () =>{
+    const geoKey = process.env.REACT_APP_GEOKEY;
+    console.log(geoKey, "This is the geocode key.");
+    const encodedAddress = encodeURIComponent(address);
+    
+    const response = await fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodedAddress + ".json?access_token="+ geoKey).then(res => res.json()).then(data => console.log(data));
+    // console.log(response, "This is the response.");
+  }
+  return(<div>
    
-    <FormControl>
+    <FormControl onSubmit={Get}>
   <InputLabel htmlFor="my-input">Enter a location</InputLabel>
-  <Input id="my-input" aria-describedby="my-helper-text" />
+  <Input onChange={changeAddress} id="my-input" aria-describedby="my-helper-text" />
   <FormHelperText id="my-helper-text">Search by city name.</FormHelperText>
-  
+ 
 </FormControl>
-<Button style={{margin: "2rem", background: "#007dc1"}}>Search</Button>
-    </div>
-)
+<Button onClick={Get} style={{margin: "2rem", background: "#007dc1"}}>Search</Button>
+    </div>)
+}
 /*function Weather() {
     return(
   <div className="app">
