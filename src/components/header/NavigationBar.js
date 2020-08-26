607c8eba9228891a4d9f24e4daea08a1c743307b
Robert Rodriguez <rrodriguez45@csustan.edu>
2020-08-25 19:37:27 -0700

    Changed navbar color

--- src/components/header/NavigationBar.js
+++ src/components/header/NavigationBar.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 const Styles = styled.div`
   .navbar {
-    background-color: #222;
+    background-color: #007dc1;
   }
   a, .navbar-brand, .navbar-nav .nav-link {
     color: #bbb;
