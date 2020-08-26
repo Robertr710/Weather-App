48c31b4c3905610d0f65649572f44c54a8496f2e
Robert Rodriguez <rrodriguez45@csustan.edu>
2020-08-25 17:31:51 -0700

    Mobile friendly update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './About.css'
 
 export const About = () => (
   <div>
     <h2>About Page</h2>
     <p>Created by Robert Rodriguez, a student at CSU Stanislaus in Turlock, California.</p>
-    <img src="/Images/profilepic.PNG" alt=""/>
+    <img  className="About" src="/Images/profilepic.PNG" alt=""/>
   </div>
 )
